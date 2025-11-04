5f49ca0b937348938324e38486a86333abe17ef9
sidharth-234 <sidzzzsidharth@gmail.com>
2025-11-04 22:52:26 +0530

    Add files via upload

---
+++ recursion_prime.py
@@ -0,0 +1,23 @@
+start=int(input('enter start range:'))
+end=int(input('enter stop range:'))
+
+def is_prime(n,i=2):
+    if n<=1:
+        return False
+    if n==i:
+        return True
+    if n%i==0:
+        return False
+    return is_prime(n,i+1)
+
+def range_prime(start,end,prime_list=None):
+    if prime_list is None:
+        prime_list=[]
+    if start>end:
+        return prime_list
+    if is_prime(start):
+        prime_list.append(start)
+    return range_prime(start+1,end,prime_list)
+prime_num=range_prime(start,end)
+print(f'prime nums in range {start} and {end} is :{prime_num}')
+    
