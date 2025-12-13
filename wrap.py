f22ec62ad461addc74d11fafcf08b11f23b4701f
sidharth-234 <sidzzzsidharth@gmail.com>
2025-12-13 20:58:26 +0530

    Add files via upload

---
+++ wrap.py
@@ -0,0 +1,15 @@
+import time
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        print(f"{func.__name__} took {time.time() - start}s")
+        return result
+    return wrapper
+
+@timer
+def test():
+    time.sleep(1)
+
+test()
